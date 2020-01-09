d940c668f3ad39a6cbdeb2f7eea2328f41dc1de1
ioszm <zmpato@yahoo.com>
2020-01-08 21:13:17 -0500

    Initial Commit

---
+++ LogInPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LogInPractice
+//
+//  Created by Zach mills on 1/8/20.
+//  Copyright © 2020 Zach mills. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
